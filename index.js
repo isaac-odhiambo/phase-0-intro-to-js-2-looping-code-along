5f53d308c7375cc154c512a3ac1e7a83f985aa2c
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-07-07 00:18:38 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesArray) {
+    let thankYouMessages = [];
+    let name =[ 'Guadalupe', 'Ollie', 'Aki']
+    for (let name of namesArray) {
+        thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
